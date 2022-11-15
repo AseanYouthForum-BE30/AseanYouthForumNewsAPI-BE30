de307ede180a544c0cff36de0c4a4e5483e5471f
Fabyan Kindarya <fkindarya@gmail.com>
2022-11-15 22:53:22 +0700

    CRUD Country

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 const authRouter = require('./auth.route')
+const countryRouter = require('./country.route')
 const userRouter = require('./user.route')
 const newsRouter = require('./news.route')
 
@@ -10,6 +11,7 @@ router.get('/', (req, res) => {
 })
 
 router.use('/auth', authRouter)
+router.use('/country', countryRouter)
 router.use('/news', newsRouter)
 router.use('/user', userRouter)
 
